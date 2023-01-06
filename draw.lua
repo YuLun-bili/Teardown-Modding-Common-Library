c4aba4f82ace2d92d9f71fdfce6b46172cea52f5
YuLun <70589524+YuLun-bili@users.noreply.github.com>
2023-01-06 17:33:36 +0000

    Create draw.lua

---
+++ draw.lua
@@ -0,0 +1,54 @@
+--	draw.lua	by YuLun
+--	Jan 06 2023
+
+function DrawAxis(trans, opt_size)
+	local size = math.abs(opt_size or 1)
+	local tPos = trans.pos
+	local Xvec = Vec(size, 0, 0)
+	local Yvec = Vec(0, size, 0)
+	local Zvec = Vec(0, 0, size)
+
+	local worldX = TransformToParentVec(trans, Xvec)
+	local worldY = TransformToParentVec(trans, Yvec)
+	local worldZ = TransformToParentVec(trans, Zvec)
+
+	DrawLine(tPos, VecAdd(tPos, worldX), 1, 0, 0)
+	DrawLine(tPos, VecAdd(tPos, worldY), 0, 1, 0)
+	DrawLine(tPos, VecAdd(tPos, worldZ), 0, 0, 1)
+end
+
+function DrawHexLine(pos0, pos1, hexColor, opt_alpha)
+	local r, g, b = HexToRGB(hexColor, false)
+	DrawLine(pos0, pos1, r, g, b, opt_alpha or 1)
+end
+
+function DrawRGBLine(pos0, pos1, RGBcolor, opt_alpha)
+	DrawLine(pos0, pos1, RGBcolor[1], RGBcolor[2], RGBcolor[3], opt_alpha or 1)
+end
+
+function DrawShapeHexOutline(handle, hexColor, opt_alpha)
+	local r, g, b = HexToRGB(hexColor, false)
+	DrawShapeOutline(handle, r, g, b, opt_alpha or 1)
+end
+
+function DrawShapeRGBOutline(handle, RGBcolor, opt_alpha)
+	DrawShapeOutline(handle, RGBcolor[1], RGBcolor[2], RGBcolor[3], opt_alpha or 1)
+end
+
+function DrawBodyHexOutline(handle, hexColor, opt_alpha)
+	local r, g, b = HexToRGB(hexColor, false)
+	DrawBodyOutline(handle, r, g, b, opt_alpha or 1)
+end
+
+function DrawBodyRGBOutline(handle, RGBcolor, opt_alpha)
+	DrawBodyOutline(handle, RGBcolor[1], RGBcolor[2], RGBcolor[3], opt_alpha or 1)
+end
+
+function DrawHexSprite(handle, trans, width, height, hexColor, opt_alpha, opt_depthTest, opt_additive)
+	local r, g, b = HexToRGB(hexColor, false)
+	DrawSprite(handle, trans, width, height, r, g, b, opt_alpha or 1, opt_depthTest, opt_additive)
+end
+
+function DrawRGBSprite(handle, trans, width, height, RGBcolor, opt_alpha, opt_depthTest, opt_additive)
+	DrawSprite(handle, trans, width, height, RGBcolor[1], RGBcolor[2], RGBcolor[3], opt_alpha or 1, opt_depthTest, opt_additive)
+end
